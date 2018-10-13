40d40bcc5030aee0461b84cd7fa9f6ca6417936a
Renan Tardelli <renantardelli@Renans-MacBook-Pro.local>
2018-10-13 09:48:35 -0300

    resumo calcula corretamente

--- resumo.py
+++ resumo.py
@@ -1,4 +1,5 @@
 #!/Users/renantardelli/miniconda2/bin python
+# -*- coding: utf-8 -*-
 
 import datetime as dt
 import pandas as pd
@@ -10,80 +11,105 @@ path = "/Users/renantardelli/Desktop/datasets/tesouro/"
 
 ### Web
 
-valores_diarios = pd.read_csv(path + 'taxas_dia.csv', parse_dates = ['date', 'vencimento'])
-
-hoje = dt.datetime.strftime(dt.datetime.today(), "%d/%m/%Y")
+valores_diarios = pd.read_csv(path + 'taxas_dia.csv', parse_dates = ['date', 'vencimento'], decimal = ',')
 
 def ajusta_nome_titulo(strng):
-	opcoes = {'Tesouro IPCA+': 'IPCA',
-	 		  'Tesouro IPCA+ com Juros Semestrais': 'IPCAJS',
-	 		  'Tesouro Prefixado': 'PRE',
-	 		  'Tesouro Prefixado com Juros Semestrais': 'PREJS',
-	 		  'Tesouro Selic': 'SELIC',
-	 		  'Tesouro IGPM+ com Juros Semestrais': 'IGPMJS'}
-	return opcoes[strng]
+    opcoes = {'Tesouro IPCA+': 'IPCA',
+              'Tesouro IPCA+ com Juros Semestrais': 'IPCAJS',
+              'Tesouro Prefixado': 'PRE',
+              'Tesouro Prefixado com Juros Semestrais': 'PREJS',
+              'Tesouro Selic': 'SELIC',
+              'Tesouro IGPM+ com Juros Semestrais': 'IGPMJS'}
+    return opcoes[strng]
 
+# Criando colunas para usar nos cáculos
 valores_diarios.loc[:, 'titulo'] = valores_diarios.titulo.map(lambda x: ajusta_nome_titulo(x[:-5]))
 valores_diarios.loc[:, 'ano'] = valores_diarios.vencimento.map(lambda x: str(x.year))
+valores_diarios.loc[:, 'data_base'] = valores_diarios.date.map(lambda x: x.strftime("%d/%m/%Y"))
+# Corrigindo para numerico
+valores_diarios.loc[:, 'preco_unitario'] = pd.to_numeric(valores_diarios.preco_unitario)
+valores_diarios.loc[:, 'taxas_dia'] = pd.to_numeric(valores_diarios.taxas_dia)
 
 ### Hist
 
-historico = pd.read_csv(path + 'taxas.csv', sep=";")
+historico = pd.read_csv(path + 'taxas.csv', sep=";", decimal=',')
 
-historico.loc[:, 'date'] = historico['Data Vencimento'].map(lambda x: dt.datetime.strptime(x, "%d/%m/%Y"))
+historico.columns = ['tipo_titulo','data_vencimento','data_base','taxa_compra_manha', 'taxa_venda_manha', 'pu_compra_manha', 'pu_venda_manha', 'pu_base_manha']
+
+historico.loc[:, 'date'] = historico['data_vencimento'].map(lambda x: dt.datetime.strptime(x, "%d/%m/%Y"))
 historico.loc[:, 'ano'] = historico.date.map(lambda x: str(x.year))
-historico.loc[:, 'base'] = historico['Data Base'].map(du.parser.parse)
-historico.loc[:, 'titulo'] = historico['Tipo Titulo'].map(ajusta_nome_titulo)
+historico.loc[:, 'base'] = historico['data_base'].map(du.parser.parse)
+historico.loc[:, 'titulo'] = historico['tipo_titulo'].map(ajusta_nome_titulo)
 historico.sort_values('base', inplace=True)
 
 
-
-### Funções mat financeira
+### Funcoes mat financeira
 
 def anualiza_taxa(i, dias):
-	return (1+i)**(250./dias) - 1
+    return (1+i)**(250./dias) - 1
 
-### Cálculos
+### Funcoes
 
+def nome_para_dict(titulo):
+    titulo,ano = titulo.split("_")
+    return {'ano':ano, 'titulo':titulo}
 
-def dados_hist(titulo, date=None):
-	# date = du.parser.parse(date)
-	titulo,ano = titulo.split("_")
-	df = historico[(historico.titulo == titulo) & (historico['Data Base'] == date) & (historico.ano == ano)]
-	return dict(df)
+def vlookup(df=historico, titulo=None, **kwargs):
+    try :
+        kwargs.update(nome_para_dict(titulo))
+    except : 
+        pass
 
-def dados_dia(titulo):
-	# date = du.parser.parse(date)
-	titulo,ano = titulo.split("_")
-	df = valores_diarios[(valores_diarios.titulo == titulo) & (valores_diarios.ano == ano)]
-	return dict(df)
+    if kwargs['data_base'] == 'max': kwargs['data_base'] = max(df['data_base'])
 
+    param = pd.DataFrame(kwargs, index=[0])
 
-dat = "31/12/2015"
-tit = 'IPCAJS_2050'
-a = dados_hist(tit, dat)
-b= dados_dia(tit)
+    return df.merge(param)
 
-def ganho(titulo, data_compra):
-	compra = dados_hist(titulo, date=data_compra)
-	venda = dados_dia(titulo)
+def simulacao_venda(titulo, data_base, data_venda='max'):
+    compra = vlookup(data_base=data_base, titulo=titulo)
+    venda = vlookup(df=valores_diarios, data_base=data_venda, titulo=titulo)
 
-	dias = (compra['date'].iloc[0] - venda['date'].iloc[0]).days
-	dif_pu = venda['preco_unitario'].iloc[0] - float(compra['PU Base Manha'].iloc[0].replace(",", "."))
-	juros = (venda['preco_unitario'].iloc[0] / float(compra['PU Base Manha'].iloc[0].replace(",", "."))) - 1
-	juros_anualizados = anualiza_taxa(juros, dias)
-	return dif_pu, dias, juros, juros_anualizados
+    dias = (venda.date - compra.base).dt.days # no historico BASE é a data do dia
 
-print ganho(tit, dat)
+    dif_pu = venda.preco_unitario - compra.pu_base_manha
+    dif_taxa = venda.taxas_dia - compra.taxa_venda_manha
 
-def valor_teorico(pu, taxa, inicio, fim=dt.datetime.today()):
-	# Assumindo que é composto diariamente
-	inicio = du.parser.parse(inicio)
-	fim = du.parser.parse(fim)
-	ts = np.cumprod(np.repeat(1+taxa, (fim-inicio).days)) * pu
- 	return ts
+    juros = (venda.preco_unitario / compra.pu_base_manha) - 1
+    juros_anualizados = anualiza_taxa(juros, dias)
 
+    return np.hstack([dias, dif_pu, dif_taxa, juros, juros_anualizados])
 
+def valor_teorico(pu, taxa, data_base, data_venda='max'):
+    # Assumindo que é composto diariamente
+    inicio = du.parser.parse(inicio)
+    fim = du.parser.parse(fim)
+    ts = np.cumprod(np.repeat(1+taxa, (fim-inicio).days)) * pu
+    return ts
 
+def mostra_resultados(titulo, data_base):
+    venda = simulacao_venda(titulo=titulo, data_base=data_base)
+    print titulo
+    print "Número de dias: %s" %(venda[0])
+    print "Ganho em valores absolutos: %.2f" %(venda[1])
+    print "Juros no período: %.2f " %(venda[3]*100)
+    print "Juros anualizados: %.2f " %(venda[4]*100)
+    print "-------------------------------------"
 
+#### TESTES
 
+# tit = "PRE_2023"
+# dat = '27/07/2017'
+
+# compra = vlookup(data_base=dat, titulo=tit)
+# venda = vlookup(df=valores_diarios, data_base=dat, titulo=tit)
+
+# simulacao_venda(tit, dat)
+
+###
+
+with open('input.json', 'r') as rf:
+    posicao = json.load(rf)
+
+for tit in range(len(posicao)):
+    mostra_resultados(posicao[tit]['titulo'], posicao[tit]['data'])
--- webparser.py
+++ webparser.py
@@ -1,3 +1,5 @@
+#!/Users/renantardelli/miniconda2/bin python
+
 import urllib2
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -12,10 +14,11 @@ page = urllib2.urlopen(url)
 soup = BeautifulSoup(page, 'html.parser')
 
 tabelas = soup.find_all('table')
-soup_taxas_dia = tabelas[1].prettify()
+soup_taxas_dia = tabelas[3].prettify()
 
 df_taxas_dia = pd.read_html(soup_taxas_dia, header=0, skiprows=1)[0].dropna(how='any')
-df_taxas_dia.columns = ['titulo', 'vencimento', 'taxas_dia', 'valor_minimo', 'preco_unitario']
+
+df_taxas_dia.columns = ['titulo', 'vencimento', 'taxas_dia', 'preco_unitario']
 
 def monetary_to_float(strng):
 	return float(strng.replace("R$", "").replace(".", "").replace(",", "."))
@@ -24,13 +27,17 @@ def fix_interest(float):
 	return float/100
 
 df_taxas_dia.loc[:, 'preco_unitario'] = df_taxas_dia.preco_unitario.map(monetary_to_float)
-df_taxas_dia.loc[:, 'valor_minimo'] = df_taxas_dia.valor_minimo.map(monetary_to_float) 
+# df_taxas_dia.loc[:, 'valor_minimo'] = df_taxas_dia.valor_minimo.map(monetary_to_float) 
 df_taxas_dia.loc[:, 'taxas_dia'] = df_taxas_dia.taxas_dia.map(fix_interest) 
 df_taxas_dia.loc[:, 'vencimento'] = df_taxas_dia.vencimento.map(du.parser.parse)
 df_taxas_dia.loc[:, 'date'] = dt.datetime.today().replace(hour=0, minute= 0, second=0, microsecond=0)
 
-print "Data base: "
+
+print "Data base: " 
+
 
 path = "/Users/renantardelli/Desktop/datasets/tesouro/"
 
 df_taxas_dia.to_csv(path + '/taxas_dia.csv', index=False)
+
+#p_p_id_precosetaxas_WAR_tesourodiretoportlet_ > div > div > div > b
